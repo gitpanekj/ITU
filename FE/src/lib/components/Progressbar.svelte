267aa9e2b8455e3f76bea0e734e1030f3e824ecb
Lucie Klímová <xklimo04@vutbr.cz>
2024-10-27 21:01:11 +0100

    feat(FE): add Progressbar and Spinner components

---
+++ FE/src/lib/components/Progressbar.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    export let currentIndex: number;
+    export let total: number;
+</script>
+
+<p class="text-center text-l">{currentIndex} / {total}</p>
+<div class="w-4/5 h-4 bg-gray-300 rounded-full mx-auto mb-4">
+    <div
+      class="h-full rounded-full transition-all duration-300"
+      style="width: {(currentIndex / total) * 100}%; background-color: #4AD0EE"
+    ></div>
+</div>
+  
---
+++ FE/src/lib/components/Spinner.svelte
@@ -0,0 +1,3 @@
+<div class="flex justify-center items-center h-40">
+    <div class="w-10 h-10 border-4 border-t-4 border-gray-300 rounded-full animate-spin" style="border-top-color: #4AD0EE;"></div>
+</div>
