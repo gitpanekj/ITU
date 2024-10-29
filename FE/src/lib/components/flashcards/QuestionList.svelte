76529c4857dec05f26d2f4f777537e8a93962085
Lucie Klímová <xklimo04@vutbr.cz>
2024-10-29 17:09:36 +0100

    feat: add hardcount to flashcard list

--- FE/src/lib/components/flashcards/QuestionList.svelte
+++ FE/src/lib/components/flashcards/QuestionList.svelte
@@ -27,8 +27,8 @@
       method: "POST",
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-		frontFace: "",
-		backFace: "",
+        frontFace: "",
+        backFace: "",
 		flashcardExerciseId: flashcardExerciseId})
      });
     await fetchQuestions();
@@ -42,7 +42,8 @@
       },
       body: JSON.stringify({
         frontFace: frontFace,
-        backFace: backFace })
+        backFace: backFace
+      })
     });
     await fetchQuestions();
   };
@@ -53,38 +54,44 @@
 <div class="h-full flex flex-col justify-between mt-10">
   <div>
     <button
-    on:click={() => {addQuestion();}}
+      on:click={() => {addQuestion();}}
       class="border-2 border-blue-900 hover:bg-blue-900 hover:text-blue-200 py-2 px-4 rounded-full bg-blue-500">
       Přidat kartu
     </button>
     
-    <!-- Question List Container -->
-    <div class="flex flex-col overflow-y-auto h-full w-4/5 gap-4 items-center mx-auto my-auto pt-10">
+    <!-- Flashcard List Container -->
+    <div class="flex flex-col overflow-y-auto max-h-[800px] h-full w-4/5 gap-4 items-center mx-auto my-auto pt-10">
+      <!-- Table Header -->
+      <div class="w-full flex justify-between p-5 font-semibold pr-[250px]">
+        <span>Líc</span>
+        <span>Rub</span>
+        <span>Těžké</span>
+      </div>
+
       {#each questions as question}
         <div class="flex w-full gap-4 items-start">
           <div class="w-full min-h-12 border-2 border-black rounded-lg flex gap-2 items-center p-4 cursor-pointer hover:bg-slate-200">
             
-            <!-- Editable FrontFace Input -->
             <textarea
-            class="w-full border-2 p-2 rounded-lg focus:outline-none focus:border-blue-500 text-xl min-h-40"
+            class="w-full border-2 p-2 rounded-lg focus:outline-none focus:border-blue-500 text-xl min-h-[125px]"
             placeholder="Zadejte líc karty..."
             bind:value={question.frontFace}
             on:blur={() => updateQuestion(question.id, question.frontFace, question.backFace)}
           ></textarea>
             
-            
           <textarea
-          class="w-full border-2 p-2 rounded-lg focus:outline-none focus:border-blue-500 text-xl min-h-40"
+          class="w-full border-2 p-2 rounded-lg focus:outline-none focus:border-blue-500 text-xl min-h-[125px]"
           placeholder="Zadejte rub karty..."
           bind:value={question.backFace}
           on:blur={() => updateQuestion(question.id, question.frontFace, question.backFace)}
         ></textarea>
+
+            <p class="mx-20">{question.hardCount}</p>
             
-            <!-- Delete Button -->
             <button
               on:click={(event) => { deleteQuestion(question.id); event.stopPropagation(); }}
               class="border-2 border-blue-900 hover:bg-red-900 hover:text-red-200 py-1 px-2 rounded-full bg-red-500">
-                Odstranit
+              Odstranit
             </button>
             
           </div>
