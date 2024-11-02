f8e3c3eac593a725b62a25c77373c428cb3f550d
Lucie Klímová <xklimo04@vutbr.cz>
2024-11-02 11:57:27 +0100

    style: flashcards - removed borders and inner scroll bar

--- FE/src/lib/components/flashcards/QuestionList.svelte
+++ FE/src/lib/components/flashcards/QuestionList.svelte
@@ -62,14 +62,14 @@
   <div>
     <button
       on:click={() => {addQuestion();}}
-      class="border-2 border-blue-900 hover:bg-blue-900 hover:text-blue-200 py-2 px-4 rounded-full bg-blue-500">
+      class="hover:bg-blue-900 hover:text-blue-200 py-2 px-4 rounded-full bg-blue-500">
       Přidat kartu
     </button>
     
     <!-- Flashcard List Container -->
-    <div class="flex flex-col overflow-y-auto max-h-[800px] h-full w-4/5 gap-4 items-center mx-auto my-auto pt-10">
+    <div class="flex flex-col overflow-y-auto h-full w-4/5 gap-4 items-center mx-auto pt-10">
       <!-- Table Header -->
-      <div class="w-full flex justify-between p-5 font-semibold pr-[280px]">
+      <div class="w-full flex justify-between pl-5 font-semibold pr-[280px]">
         <span>Líc</span>
         <span>Rub</span>
         <span>Těžké</span>
@@ -77,7 +77,7 @@
 
       {#each questions as question}
         <div class="flex w-full gap-4 items-start">
-          <div class="w-full min-h-12 border-2 border-black rounded-lg flex gap-2 items-center p-4">
+          <div class="w-full min-h-12 border-2 border-black bg-background rounded-lg flex gap-2 items-center p-4">
             
             <textarea
             class="w-full border-2 p-2 rounded-lg focus:outline-none focus:border-blue-500 text-xl min-h-[125px]"
@@ -97,12 +97,12 @@
             <div class="flex flex-col gap-5">
             <button
               on:click={(event) => { deleteQuestion(question.id); event.stopPropagation(); }}
-              class="border-2 border-blue-900 hover:bg-red-900 hover:text-red-200 py-1 px-2 rounded-full bg-red-500">
+              class="hover:bg-red-900 hover:text-red-200 py-1 px-2 rounded-full bg-red-500">
               Odstranit
             </button>
             <button
               on:click={() => {showFeedback(question.id);}}
-              class="border-2 border-blue-900 hover:bg-blue-900 hover:text-blue-200 py-2 px-4 rounded-full bg-blue-500">
+              class="hover:bg-blue-900 hover:text-blue-200 py-2 px-4 rounded-full bg-blue-500">
               Poznámky
             </button>
             </div>
@@ -114,8 +114,8 @@
 
   <!-- Pagination Controls -->
   <div class="w-full border-black border-4 h-12 flex gap-4 justify-center items-center">
-    <button on:click={() => { if (page > 1) page -= 1; fetchQuestions(); }}>Prev</button>
+    <button on:click={() => { if (page > 1) page -= 1; fetchQuestions(); }}>Předchozí</button>
     <h1>{page}/{totalPages}</h1>
-    <button on:click={() => { if (page < totalPages) page += 1; fetchQuestions(); }}>Next</button>
+    <button on:click={() => { if (page < totalPages) page += 1; fetchQuestions(); }}>Další</button>
   </div>
 </div>
