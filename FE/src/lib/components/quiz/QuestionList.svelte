c63db2b663d7141db1b4193a726d760c7fc972b6
jirikantor <j.kan3@seznam.cz>
2024-12-08 22:03:15 +0100

    feat: auto-saving functionality for quiz question detail + comments

--- FE/src/lib/components/quiz/QuestionList.svelte
+++ FE/src/lib/components/quiz/QuestionList.svelte
@@ -1,33 +1,48 @@
+<!-------------------------------------------------------------- 
+Filename: FE/src/lib/components/quiz/QuestionList.svelte
+Author: Jiří Kantor
+Login: xkanto16
+Last Modified: [08-12-2024]
+Description: Component obtaining fetching and manipulation with questions in list form
+---------------------------------------------------------------->
+
 <script lang="ts">
+  // library import
   import { onMount } from "svelte";
   import { createEventDispatcher } from 'svelte';
 
+  // property
   export let quizId: number;
-  let selectedQuestionId: number | null = null;
+  export let selectedQuestionId: number | null;
+
+  // helper UI vars
+  let questions: any = [];
+  let page: number = 1;
 
   const dispatch = createEventDispatcher();
 
-  // Function to handle selecting a question and dispatching the event
+  // handling selected question and informing other components
   const selectQuestion = (id: number) => {
     selectedQuestionId = id;
     dispatch('selectQuestion', { selectedQuestionId }); 
   };
 
-  let questions: any = [];
-  let page: number = 1;
-
-
+  // data fetch
   const fetchQuestions = async () => {
     const response = await fetch(`http://localhost:3000/quiz-exercise/question?quizId=${quizId}&page=${page}`);
     const { data, total } = await response.json();
     questions = data;
 
-    
+    // select first question if nothing selected
     if (questions.length > 0) {
-      selectQuestion(questions[0].id);
+      if (selectedQuestionId == null) {
+          selectQuestion(questions[0].id);
+      }
+      
     }
   };
 
+  // question adding
   const addQuestion = async () => {
     const response = await fetch(`http://localhost:3000/quiz-exercise/question`, {
       method: "POST",
@@ -37,18 +52,21 @@
       body: JSON.stringify({ name: "Nová otázka", question: "Text otázky", quizId: quizId, rightAnswerId: null })
     });
     const data = await response.json();
+    selectQuestion(data.id)
     await createNewAnswer(data.id);
     await fetchQuestions();
   };
 
+  // question deletion
   const deleteQuestion = async (id: number) => {
     const response = await fetch(`http://localhost:3000/quiz-exercise/question/${id}`, {
       method: "DELETE"
     });
-
+    selectedQuestionId = null;
     await fetchQuestions();
   };
 
+  // answer adding
   const createNewAnswer = async (newQuestionId: number) => {
     const response = await fetch(`http://localhost:3000/quiz-exercise/answer/`, {
         method: "POST",
@@ -61,6 +79,7 @@
     await setCorrectAnswer(newQuestionId, data.id);
   };
 
+  // answer setting as correct
   const setCorrectAnswer = async (newQuestionId: number, correctAnswerId: number) => {
     const response = await fetch(`http://localhost:3000/quiz-exercise/question/${newQuestionId}`,
     {
@@ -77,26 +96,26 @@
   });
 </script>
 
-
+<!-- header with add question button -->
 <div class="flex justify-between">
-<h1 class="text-3xl mb-4">Otázky</h1>
-<button 
-  on:click={() => { addQuestion(); }}
-  class="border-2 border-blue-900 hover:bg-blue-600 hover:text-blue-200 py-2 px-4 size-12 rounded-full bg-blue-950 text-white text-xl text-justify">
-    +
-</button>
+  <h1 class="text-3xl mb-4">Otázky</h1>
+  <button 
+    on:click={() => { addQuestion(); }}
+    class="border-2 border-blue-900 hover:bg-blue-600 hover:text-blue-200 py-2 px-4 size-12 rounded-full bg-blue-950 text-white text-xl text-justify">
+      +
+  </button>
 </div>
 <hr class="h-1 mt-4 bg-blue-950 border-0 dark:bg-blue-950 w-11/12 overflow-y-auto">
 
+<!-- question list header -->
 <div class="w-11/12 flex justify*between items-center mt-6">
   <div class="ml-4 text-4xl w-96 font-bold text-start">Název</div>
   <div class="w-36 text-2xl">Správně</div>
   <div class="w-36 text-2xl">Špatně</div>
   <div class="w-36 text-2xl">Těžké</div>
-
 </div>
 
-
+<!-- question list -->
 <div class="flex flex-col mt-0 pt-4 gap-2 overflow-y-auto max-h-full" style="max-height: 80vh;">
 {#each questions as question (question.id)}
   <button 
@@ -106,11 +125,13 @@
     : 'hover:bg-gray-300 hover:border-blue-200 border-blue-950'}`}
     on:click={() => selectQuestion(question.id)}>
 
+    <!-- question info items -->
     <div class="text-2xl w-96 text-start">{question.name}</div>
     <div class="w-36 text-2xl">{question.noCorrect}</div>
     <div class="w-36 text-2xl">{question.noWrong}</div>
     <div class="w-36 text-2xl">{question.hardCount}</div>
     
+    <!-- delete button -->
     <button on:click={ async ()  => { await deleteQuestion(question.id);}} class="m-1 p-1 rounded-full bg-transparent hover:bg-red-400 border-0">
       <img
         alt="Smazat otázku"
--- FE/src/lib/components/quiz/QuizEvaluation.svelte
+++ FE/src/lib/components/quiz/QuizEvaluation.svelte
@@ -1,13 +1,21 @@
+<!-------------------------------------------------------------- 
+Filename: FE/src/lib/components/quiz/QuizEvaluation.svelte
+Author: Jiří Kantor
+Login: xkanto16
+Last Modified: [08-12-2024]
+Description: Component for view of the evaluated quiz
+---------------------------------------------------------------->
+
 <script lang="ts">
+    // library import
     import { onMount } from "svelte";
     import { userView } from "../../../stores/Quiz/userView";
     
+    // helper UI vars
     let wrongQuestions: any = [];
     let correctQuestions: any = [];
     let noCorrect: number = 0;
     let noWrong: number = 0;
-    let totalCount: number = 0;
-    let successRate: number = 0;
 
     // vars for details of question
     let correctAnswerId: number | null = null;
@@ -20,7 +28,7 @@
     let showDetail: boolean = false;
     
 
-
+    // load question by id
     const loadQuestion = async (id: number) => {
         const response = await fetch(`http://localhost:3000/quiz-exercise/question/${id}`);
         const data = await response.json();
@@ -30,12 +38,14 @@
 
     };
 
+    // load question answers by question id
     const loadQuestionAnswers = async (id: number) => {
         const response = await fetch(`http://localhost:3000/quiz-exercise/answer?questionId=${id}`);
         const {data, total} = await response.json();
         answers = data;
     };
 
+    // session evaluation
     onMount(async () => {
         const response = await fetch(`http://localhost:3000/quiz-exercise/evaluate_session/${localStorage.getItem('quizSessionId')}`,
             {
@@ -50,25 +60,18 @@
         correctQuestions = data.correct;
         noCorrect = data.no_correct;
         noWrong = data.no_wrong;
-        totalCount = noCorrect + noWrong;
-        successRate = noCorrect / totalCount;
-        console.log("Data: ", data, "celkem otázek:", totalCount, "success rate: ", successRate);
     });
 
 
 
 </script>
-{@debug correctQuestions}
-{@debug wrongQuestions}
 
 <!-- two-column page with questions left - correct ones, right - wrong ones -->
-<!-- or on the left all and on the right detail of chosen one -->
 <div class="flex flex-col v-screen h-1/8 p-8 border-a border-black border-0">
     <div class="text-4xl mb-4">Vyhodnocení</div>
 
 </div>
 <div class="flex h-screen">
-       
 
     <!-- Left column: Correct questions -->
     <div class="w-1/2 h-full p-8 border-a border-black border-0 justify-between">
@@ -78,23 +81,25 @@
         </h1>
         <hr class="h-1 mt-4 bg-blue-950 border-0 dark:bg-blue-950 w-11/12 overflow-y-auto">
 
+        <!-- question list -->
         <div class="flex flex-col mt-0 pt-4 gap-2 overflow-y-auto max-h-full" style="min-height: 80vh;">
             {#each correctQuestions as correctQuestion}
-                <div 
+                <!-- on question click shows question detailed evaluation -->
+                <button 
                     class={`w-11/12 h-14 px-4 border-4 border-blue-950 flex justify-between items-center rounded-lg cursor-pointer hover:bg-gray-300 hover:border-blue-200`}
-                    >
-                    <div class="text-2xl w-96 text-start">{correctQuestion.name}</div>
-
-                    <button on:click={ async () => { 
+                    on:click={ async () => { 
                         showDetail = true;
                         chosen = null;
                         questionDetail = correctQuestion;
                         await loadQuestion(correctQuestion.id);
                         await loadQuestionAnswers(correctQuestion.id);
-                        }}
-                        class="border-2 bg-blue-950 text-white hover:bg-blue-600 rounded-full px-2 py-1">Detail</button>
+                        }}>
+                    <div class="text-2xl w-96 text-start">{correctQuestion.name}</div>
 
-                </div>
+
+                    <button class="border-2 bg-blue-950 text-white hover:bg-blue-600 rounded-full px-2 py-1">Detail</button>
+
+                    </button>
             {/each}
         </div>
     </div>
@@ -107,40 +112,68 @@
         </h1>
         <hr class="h-1 mt-4 bg-blue-950 border-0 dark:bg-blue-950 w-11/12 overflow-y-auto">
 
+        <!-- question list -->
         <div class="flex flex-col mt-0 pt-4 gap-2 overflow-y-auto max-h-full" style="max-height: 80vh;">
             {#each wrongQuestions as wrongQuestion}
-                <div 
-                    class={`w-11/12 h-14 px-4 border-4 border-blue-950 flex justify-between items-center rounded-lg cursor-pointer hover:bg-gray-300 hover:border-blue-200`}
-                    >
+                <!-- on question click shows question detailed evaluation -->
+                <button class={`w-11/12 h-14 px-4 border-4 border-blue-950 flex justify-between items-center rounded-lg cursor-pointer hover:bg-gray-300 hover:border-blue-200`}
+                        on:click={ async () => {
+                            showDetail = true;
+                            chosen = wrongQuestion.chosen;
+                            questionDetail = wrongQuestion;
+                            await loadQuestion(wrongQuestion.id);
+                            await loadQuestionAnswers(wrongQuestion.id);
+                            }}>
                     <div class="text-2xl w-96 text-start">{wrongQuestion.name}</div>
 
-                    <button on:click={ async () => {
-                        showDetail = true;
-                        chosen = wrongQuestion.chosen;
-                        questionDetail = wrongQuestion;
-                        await loadQuestion(wrongQuestion.id);
-                        await loadQuestionAnswers(wrongQuestion.id);
-                        }}
-                        class="border-2 bg-blue-950 text-white hover:bg-blue-600 rounded-full px-2 py-1">Detail</button>
-                </div>
+                    <!-- show detail button -->
+                    <button class="border-2 bg-blue-950 text-white hover:bg-blue-600 rounded-full px-2 py-1">Detail</button>
+                </button>
             {/each}
         </div>
     </div>
 
 </div>
 
+<!-- question detail -->
 {#if showDetail}
-    <!-- Overlay -->
-    <div class="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-        <!-- Detail Window -->
-        <div class="h-2/3 w-1/2 bg-blue-950 shadow-lg shadow-blue-900/50 text-white text-4xl rounded-xl p-8 max-w-2xl relative">
+    <!-- darkened background -->
+    <div 
+        role="button"
+        tabindex="0"
+        on:click={() => {
+            showDetail = false;
+            chosen = null;
+        }}
+        on:keydown={(e) => {
+            if (e.key === 'Enter' || e.key === ' ') {
+                showDetail = false;
+                chosen = null;
+            }
+        }}
+        class="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50"
+    >
+
+        <!-- detail window - propagation of the of-lick detail close is stopped-->
+        <div
+            class="h-2/3 w-1/2 bg-blue-950 shadow-lg shadow-blue-900/50 text-white text-4xl rounded-xl p-8 max-w-2xl relative"
+            role="button"
+            tabindex="0"
+            on:click|stopPropagation ={() => {
+                showDetail = true;
+            }}
+            on:keydown={(e) => {
+                showDetail = true;
+            }}
+        >
             <h2 class="text-3xl mb-4">{questionDetail.name}</h2>
             <div class="mt-10 h-1/4 rounded-xl border-4 text-white border-neutral-400 text-2xl p-2">{questionText}</div>
             
+            <!-- question answers -->
             <div class="text-2xl mt-12 space-y-6 text-black w-full overflow-y-auto max-h-full">
                 {#each answers as answer}
                     
-                    
+                    <!-- answer correct/wrong/chosen/other colors -->
                     <button  
                         class={`border-1 border-color-zinc-400 flex rounded-xl w-full h-auto cursor-pointer 
                         ${correctAnswerId === answer.id 
@@ -152,6 +185,8 @@
                     </button>                
                 {/each}
             </div>
+
+            <!-- close button -->
             <button on:click={() => {
                 showDetail = false;
                 chosen = null;
